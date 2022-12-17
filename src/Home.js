0a83b106b672d5a03e610e228bbab3468f8a5f26
Theophilus <theophilusadesola002@gmail.com>
2022-12-17 14:21:24 +0100

    React Props properly explained

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,22 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-	const [name, setName] = useState("Theo");
-	const [age, setAge] = useState(15);
-
-	const handleClick = () => {
-		setName("Philus");
-		setAge(20);
-	};
+	const [blogs, setBlogs] = useState([
+		{ title: "Electromechanical Devices", body: "Electromechanical devices are hybrid devices that put to good use to important forms of energy.", author: "Professor Theophilus", id: 1 },
+		{ title: "Circuit Analysis", body: "Electrical circuits are pathways for electric current to flow.", author: "Robotron", id: 2 },
+		{
+			title: "Introduction to Robotics Programming",
+			body: "Robotic programming is best seen as a way you give set of commands to a machine so that you can sit back and watch the machine do all the dirty work.",
+			author: "Professor Theophilus",
+			id: 3,
+		},
+	]);
+	const handleClick = () => {};
 
 	return (
 		<div className="home">
-			<h2>Homepage</h2>
-			<p>
-				{name} is {age} years old
-			</p>
+			<BlogList blogsProps={blogs} title="All Blogs" />
 			<button onClick={handleClick}>Click Me</button>
 		</div>
 	);
