2285c8f646c61e5f1f70b8aa21568f5facf04702
Theophilus <theophilusadesola002@gmail.com>
2022-12-20 19:50:06 +0100

    Introduction to route parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom"
+
 const BlogList = ({ blogsProps, title }) => {
 	return (
 		<div className="blog-list">
 			<h1>{title}</h1>
 			{blogsProps.map((blog) => {
 				return (
-					<div className="blog-preview">
-						<h2>{blog.title}</h2>
+					<div className="blog-preview" key={blog.id}>
+						<Link to={`/blogs/${blog.id}`}>
+							<h2>{blog.title}</h2>
+							<p>Written by {blog.author}</p>
+						</Link>
 					</div>
 				)
 			})}
