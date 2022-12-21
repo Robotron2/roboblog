ce2599a4e0f5f4daeb2a4551bc4568302331f5f8
Theophilus <theophilusadesola002@gmail.com>
2022-12-21 21:23:28 +0100

    NotFound W-I-P

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,10 +2,26 @@ import { Link } from "react-router-dom"
 
 const NotFound = () => {
 	return (
-		<div className="not-found">
-			<h2>Sorry</h2>
-			<p>The page cannot be found.....</p>
-			<Link to={"/"}>Click to go to the homepage.....</Link>
+		// <div className="home">
+		// 	<div className="err">4</div>
+		// 	<i className="far fa-question-circle fa-spin"></i>
+		// 	<div className="err2">4</div>
+		// 	<div className="msg">
+		// 		Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?
+		// 		<p>
+		// 			Let's go <Link to={"/"}>home</Link> and try from there.
+		// 		</p>
+		// 	</div>
+		// </div>
+		<div id="pagenotfound">
+			<div className="pagenotfound">
+				<div className="notfound404">
+					<h1>Oops!</h1>
+				</div>
+				<h2>404 - Page not found</h2>
+				<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable</p>
+				<Link to={"/"}>Go to homepage</Link>
+			</div>
 		</div>
 	)
 }
