be481f177c62bd83da02b63dbae50c924c6cc952
Theophilus <theophilusadesola002@gmail.com>
2022-12-21 11:00:23 +0100

    useState implemented to control the author input

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,21 @@
+import { useState } from "react"
+
 const Create = () => {
+	const [title, setTitle] = useState("")
+
 	return (
 		<div className="create">
 			<h2>Add a New Blog</h2>
 			<form>
 				<label>Blog title:</label>
-				<input type="text" required />
+				<input
+					type="text"
+					required
+					value={title}
+					onChange={(e) => {
+						setTitle(e.target.value)
+					}}
+				/>
 				<label>Blog body:</label>
 				<textarea required></textarea>
 				<label>Blog author:</label>
@@ -14,6 +25,7 @@ const Create = () => {
 				</select>
 				<button>Add Blog</button>
 			</form>
+			<div>{title}</div>
 		</div>
 	)
 }
