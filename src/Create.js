065e11ea042e02b6567d63a284aea51ffdb8bc02
Theophilus <theophilusadesola002@gmail.com>
2022-12-21 10:49:09 +0100

    Introduction to controlled inputs

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,18 @@ const Create = () => {
 	return (
 		<div className="create">
 			<h2>Add a New Blog</h2>
+			<form>
+				<label>Blog title:</label>
+				<input type="text" required />
+				<label>Blog body:</label>
+				<textarea required></textarea>
+				<label>Blog author:</label>
+				<select>
+					<option value="Theophilus">Theophilus</option>
+					<option value="Robotron">Robotron</option>
+				</select>
+				<button>Add Blog</button>
+			</form>
 		</div>
 	)
 }
