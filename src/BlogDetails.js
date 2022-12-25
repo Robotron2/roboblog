d751a3e74be1df5f8f0682e47dbefb50f21130af
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-25 14:36:54 +0100

    Blog details can now be viewed

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -4,49 +4,48 @@ const BlogDetails = () => {
 	const history = useHistory()
 	const dbBlog = JSON.parse(localStorage.getItem("AllBlogs")) // [{}, {}, ...]
 
-	const idObject = useParams()
-	const urlId = idObject.id
+	const urlId = useParams()
 
-	const filterer = (blogItem) => {
-		return blogItem.id === urlId
-	}
-	const delfilterer = (blogItem) => {
-		return blogItem.id !== urlId
-	}
+	// const filterer = (blogItem) => {
+	// 	return blogItem.id === urlId
+	// }
+	// const delfilterer = (blogItem) => {
+	// 	return blogItem.id !== urlId
+	// }
 
 	const handleDelete = (id) => {
 		console.log(id)
-		let renewedBlogs = dbBlog.filter(delfilterer)
-		localStorage.setItem("AllBlogs", JSON.stringify(renewedBlogs))
+		// let renewedBlogs = dbBlog.filter(delfilterer)
+		// localStorage.setItem("AllBlogs", JSON.stringify(renewedBlogs))
 
 		history.push("/")
 		// console.log(renewedBlogs);
 	}
 
-	const filteredArray = dbBlog.filter(filterer)
-	// console.log(typeof(filteredArray))
-	// console.log(filteredArray[0])
-
-	return filteredArray.map((blog, index) => {
-		return (
-			<div className="blog-details" key={index + 1}>
-				{blog && (
-					<article>
-						<h2>{blog.title}</h2>
-						<p>{blog.author}</p>
-						<div>{blog.body}</div>
-					</article>
-				)}
-				<button
-					onClick={() => {
-						handleDelete(urlId)
-					}}
-				>
-					Delete Blog
-				</button>
-			</div>
-		)
+	const filteredArray = dbBlog.filter((blog) => {
+		return blog.id == urlId.id
 	})
+
+	console.log(filteredArray)
+
+	return (
+		<div>
+			{filteredArray.map((blog, index) => {
+				return (
+					<div className="blog-details" key={index + 1}>
+						<h2>{blog.title}</h2>
+
+						<p>
+							<i>written by:</i> {blog.author}
+						</p>
+						<div className="content">
+							<p>{blog.body}</p>
+						</div>
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default BlogDetails
